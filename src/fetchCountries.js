5a804025a962b33ee7eaeb37a43f25dde6e8e73f
Vitalii Speka <spekava@gmail.com>
2020-11-14 20:10:18 +0200

    дабавил разметку страны, настроил отображение инпута
    ЕЩЕ!
    добавть класс API

---
+++ src/fetchCountries.js
@@ -0,0 +1,15 @@
+export default class FetchApiCountries {
+    constructor() { }
+    
+    fetchCountries() {
+        return fetch(`https://restcountries.eu/rest/v2/name/${this.searchQuery}`)
+        .then(response => response.json())
+        .then(countries => { return countries})
+        .catch(error => { console.log(error) });
+    }
+
+    get query() {
+        return this.searchQuery;
+    }
+
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
 import './styles.css';
+import countiesTpl from './templates/countries.hbs';
+import FetchApiCountries from './fetchCountries';
 
+const debounce = require('lodash.debounce');
 
-console.log(`rest test2`);
 console.log(`hw-12-countries`);
+
+const fetchApiCounries = mew FetchApiCountries();
+
+const counriesContainer = document.querySelector('.countries');
+const searchForm = document.querySelector('.js-search-form')
+
+searchForm.addEventListener('input', debounce(onSearch, 500));
+
+function onSearch(e) {
+    e.preventDefault();
+    // console.log(e.currentTarget.elements.query.value);
+    fetchApiCounries.query = e.target.value;
+    
+    
+    // const searchQuery = e.target.value;
+    // const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
+    // console.log(searchQuery);
+    // fetch(url)
+    // .then(response => response.json())
+    //     .then(countries => {
+    //         const markup = countiesTpl(countries);
+    //         counriesContainer.insertAdjacentHTML('beforeend', markup);
+    // })
+    // .catch(error => { console.log(error) });
+}
+
+// console.log(searchForm);
+// counriesContainer.insertAdjacentHTML('beforeend', countiesTpl);
+// counriesContainer.innerHTML = countiesTpl;
+// console.log(countiesTpl);
