907aa3cdf91418d3e8ff477b9b3df5d119d956bb
Vitalii Speka <spekava@gmail.com>
2020-11-15 18:58:33 +0200

    выполнил. осталось тестировать

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import './styles.css';
-import countiesTpl from './templates/countries.hbs';
+import countiesOneTpl from './templates/countries-one.hbs';
+import countiesManyTpl from './templates/countries-many.hbs';
 import FetchApiCountries from './fetchCountries';
+import '@pnotify/core/dist/BrightTheme.css';
 
-
+const { error } = require('@pnotify/core');
 const debounce = require('lodash.debounce');
-
-console.log(`hw-12-countries`);
-
 const fetchApiCounries = new FetchApiCountries();
 
 const countriesContainer = document.querySelector('.js-countries-container');
@@ -16,42 +15,33 @@ searchForm.addEventListener('input', debounce(onSearch, 1000));
 
 function onSearch(e) {
     e.preventDefault();
-    // console.log(e.currentTarget.elements.query.value);
+    clearCountriesContainer();
     fetchApiCounries.query = e.target.value.trim();
 
-    // fetchApiCounries.fetchCountries().then(countries => {
-    //     countriesContainer.innerHTML = countiesTpl(countries);
-    // }
-
-            
     fetchApiCounries.fetchCountries().then(countries => {
-        if (countries.length === 1) {
-            console.log('===1');
-            countriesContainer.innerHTML = countiesTpl(countries);
-        } else if (countries.length >= 2 && countries.length <= 10) {
-            console.log('>2 && <10');
-        } else if (countries.length > 10) {
-            console.log('>10');
+        if (countries.length > 10) {
+            error({
+                text: "Более 10 стран. Пожалуйста, уточните запрос!"
+            });
+        } else if (countries.status === 404) {
+            error({
+            text: "Страна не найдена. Пожалуйста, уточните запрос!"
+        });
+        } else if (countries.length === 1) {
+            countriesContainer.innerHTML = countiesOneTpl(countries);
+        //     } elseif (countries.length >= 2 && countries.length <= 10)
+        } else if (countries.length <= 10) {
+            countriesContainer.innerHTML = countiesManyTpl(countries);
         }
-        }  
-        );
+    })
+    .catch(Error => {
+        Error({
+            text: "You must enter query parameters!"
+        });
+        console.log(Error)
+    })
 }
 
-function countriesMarkupNew (countries) {
+function clearCountriesContainer() {
+    countriesContainer.innerHTML = '';
 }
-
-function countriesMarkup(countries) {
-    counriesContainer.insertAdjacentHTML('beforeend', countiesTpl(countries));
-    // if (countries.length === 1) {
-    //     counriesContainer.insertAdjacentHTML('beforeend', markup);
-    //     // counriesContainer.insertAdjacentHTML('beforeend', countiesTpl(countries));
-    //      console.log('===1');
-    // } else if (countries.length >= 2 && countries.length <= 10) {
-    //     console.log('>2 && <10');
-    // } else if (countries.length > 10) {
-    //     console.log('>10');
-    // }
-}
-
-// console.log(searchForm);
-// console.log(countiesTpl);
