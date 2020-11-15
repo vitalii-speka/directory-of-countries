22b33a41a993541ac9fa54dde9b1a60b9ce7a8d4
Vitalii Speka <spekava@gmail.com>
2020-11-15 11:24:02 +0200

    еще
    - нотифай
    - привести в порядок

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ console.log(`hw-12-countries`);
 
 const fetchApiCounries = new FetchApiCountries();
 
-const counriesContainer = document.querySelector('.countries');
+const countriesContainer = document.querySelector('.js-countries-container');
 const searchForm = document.querySelector('.js-search-form')
 
 searchForm.addEventListener('input', debounce(onSearch, 1000));
@@ -19,21 +19,38 @@ function onSearch(e) {
     // console.log(e.currentTarget.elements.query.value);
     fetchApiCounries.query = e.target.value.trim();
 
-    fetchApiCounries.fetchCountries().then(countriesMarkup);
+    // fetchApiCounries.fetchCountries().then(countries => {
+    //     countriesContainer.innerHTML = countiesTpl(countries);
+    // }
+
+            
+    fetchApiCounries.fetchCountries().then(countries => {
+        if (countries.length === 1) {
+            console.log('===1');
+            countriesContainer.innerHTML = countiesTpl(countries);
+        } else if (countries.length >= 2 && countries.length <= 10) {
+            console.log('>2 && <10');
+        } else if (countries.length > 10) {
+            console.log('>10');
+        }
+        }  
+        );
+}
+
+function countriesMarkupNew (countries) {
 }
 
 function countriesMarkup(countries) {
-    console.log('dddd');
-    const markup = countiesTpl(countries[0]);
-    if (countries.lenght === 1) {
-        counriesContainer.insertAdjacentHTML('beforeend',markup);
-        // counriesContainer.insertAdjacentHTML('beforeend', countiesTpl(countries));
-    } else if (countries.lenght >= 2 && countries.lenght <= 10) {
-        console.log('>2 && <10');
-    } else if (countries.lenght > 10) {
-        console.log('>10');
-    }
-    
+    counriesContainer.insertAdjacentHTML('beforeend', countiesTpl(countries));
+    // if (countries.length === 1) {
+    //     counriesContainer.insertAdjacentHTML('beforeend', markup);
+    //     // counriesContainer.insertAdjacentHTML('beforeend', countiesTpl(countries));
+    //      console.log('===1');
+    // } else if (countries.length >= 2 && countries.length <= 10) {
+    //     console.log('>2 && <10');
+    // } else if (countries.length > 10) {
+    //     console.log('>10');
+    // }
 }
 
 // console.log(searchForm);
