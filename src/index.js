64ea95113817a11100de5ccc01210773bb86dcc1
Vitalii Speka <spekava@gmail.com>
2020-11-16 17:44:36 +0200

    исправил!
    ДЗ готово

--- src/index.js
+++ src/index.js
@@ -13,37 +13,44 @@ const searchForm = document.querySelector('.js-search-form')
 
 searchForm.addEventListener('input', debounce(onSearch, 500));
 
+
 function onSearch(e) {
     e.preventDefault();
     clearCountriesContainer();
-    // .trim()
+
     fetchApiCounries.query = e.target.value.trim();
-  
-    fetchApiCounries.fetchCountries().then(countries => {
-        if (countries.length > 10) {
+
+    if (fetchApiCounries.query.length === 0) {
+        return
+    } else {
+        fetchApiCounries.fetchCountries().then(countries => {
+        if (e.target.value === 0) {
+            return
+        }
+        else if (countries.length > 10) {
             error({
                 text: "Более 10 стран. Пожалуйста, уточните запрос(1)!",
                 delay: 1000,
             });
-            // (countries.status === 404)
         } else if (countries.status === 404) {
             error({
             text: "Страна не найдена. Пожалуйста, уточните запрос(404)!"
         });
         } else if (countries.length === 1) {
             countriesContainer.innerHTML = countiesOneTpl(countries);
-        //     } elseif (countries.length >= 2 && countries.length <= 10)
+        // (countries.length >= 2 && countries.length <= 10)
         } else if (countries.length <= 10) {
             countriesContainer.innerHTML = countiesManyTpl(countries);
         }
-    })
-    .catch(Error => {
-        Error({
-            text: "+catch+ You must enter query parameters!",
-            delay: 3000
-        });
-        console.log(Error)
-    })
+        })
+        .catch(Error => {
+            Error({
+                text: "+catch+ You must enter query parameters!",
+                delay: 3000
+            });
+            console.log(Error)
+        })
+    }
 }
 
 function clearCountriesContainer() {
